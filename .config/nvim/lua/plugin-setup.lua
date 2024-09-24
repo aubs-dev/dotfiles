5e11e69d048f1c928f16cf5f87bd81edcc5cb7cb
A. Dominik <hello@aubs.dev>
2024-09-24 01:29:00 +0100

    feat: added exclusion to "assets" from telescope search in nvim

--- .config/nvim/lua/plugin-setup.lua
+++ .config/nvim/lua/plugin-setup.lua
@@ -56,7 +56,7 @@ require("lazy").setup({
 						"--hidden",
 						"--no-ignore-vcs",
 						"--glob",
-						"!{**/.git/*,**/.cache/*,**/bin/*,**/dist/*,**/build/*,**/external/*}",
+						"!{**/.git/*,**/.cache/*,**/bin/*,**/dist/*,**/build/*,**/assets/*,**/external/*}",
 					},
 				},
 				pickers = {
@@ -69,7 +69,7 @@ require("lazy").setup({
 							"--files",
 							"--no-ignore-vcs",
 							"--glob",
-							"!{**/.git/*,**/.cache/*,**/bin/*,**/dist/*,**/build/*}",
+							"!{**/.git/*,**/.cache/*,**/bin/*,**/dist/*,**/build/*,**/assets/*}",
 						},
 					},
 				},
