3e2912594f98768f4bc5d4306b6d84d388af5d99
A. Dominik <hello@aubs.dev>
2025-03-02 12:43:45 +0000

    feat: added nvim discord presence

--- .config/nvim/lua/plugin-setup.lua
+++ .config/nvim/lua/plugin-setup.lua
@@ -261,4 +261,63 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "vyfor/cord.nvim",
+        build = ":Cord update",
+        config = function()
+            require('cord').setup({
+                editor = {
+                    tooltip = "Nvim <3",
+                },
+                display = {
+                    theme = "material",
+                    swap_icons = true,
+                },
+                timestamp = {
+                    enabled = true,
+                    reset_on_change = false,
+                },
+                idle = {
+                    enabled = false,
+                },
+                text = {
+                    workspace = function(opts) return "in " .. opts.workspace .. "." end,
+                    viewing = function(opts) return "Viewing " .. opts.filename end,
+                    editing = function(opts) return "Editing " .. opts.filename end,
+                    file_browser = true,
+                    plugin_manager = true,
+                    lsp = true,
+                    docs = true,
+                    vcs = true,
+                    notes = true,
+                    debug = true,
+                    test = true,
+                    diagnostics = true,
+                    games = true,
+                    terminal = true,
+                    dashboard = true,
+                },
+                assets = {
+                    [".h"] = {
+                        icon = require("cord.api.icon").get("c", "material"),
+                        tooltip = "C Header",
+                        name = "C Header",
+                        type = "language",
+                    },
+                },
+                advanced = {
+                    plugin = {
+                        cursor_update = "none",
+                    },
+                    discord = {
+                        reconnect = {
+                            enabled = false,
+                            interval = 5000,
+                            initial = true,
+                        },
+                    },
+                },
+            })
+        end
+    }
 })
